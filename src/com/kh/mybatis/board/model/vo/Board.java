206ff9bad1b657b30ef0003bb641f53809b06bed
최명진 <dunghasd@gmail.com>
2023-05-17 09:45:00 +0900

    no message

--- src/com/kh/mybatis/board/model/vo/Board.java
+++ src/com/kh/mybatis/board/model/vo/Board.java
@@ -2,32 +2,18 @@ package com.kh.mybatis.board.model.vo;
 
 import java.sql.Date;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
 public class Board {
 
 	private int boardNo;
 	private String boardTitle;
-	private String boardContent;
 	private String boardWriter;
 	private int count;
 	private Date createDate;
-	private String status;
-
-	public Board() {
-	}
-
-	public Board(int boardNo, String boardTitle, String boardContent, String boardWriter, int count, Date createDate,
-			String status) {
-		super();
-		this.boardNo = boardNo;
-		this.boardTitle = boardTitle;
-		this.boardContent = boardContent;
-		this.boardWriter = boardWriter;
-		this.count = count;
-		this.createDate = createDate;
-		this.status = status;
-	}
+	
+	
 
 }
